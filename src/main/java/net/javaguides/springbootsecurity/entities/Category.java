75cfad147b01e786fbeb64a2f4ccef65217b348d
Daniil <danik22122005@gmail.com>
2020-07-08 22:16:27 +0300

    first

---
+++ src/main/java/net/javaguides/springbootsecurity/entities/Category.java
@@ -0,0 +1,31 @@
+package net.javaguides.springbootsecurity.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
