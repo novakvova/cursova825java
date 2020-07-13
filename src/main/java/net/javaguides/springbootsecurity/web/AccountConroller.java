3264cb8ec0cbf1f2a9e7df572e650e04fc4f9166
Volodymyr Novak <novakvova@gmail.com>
2020-07-13 15:29:39 +0300

    Add register user

--- src/main/java/net/javaguides/springbootsecurity/web/AccountConroller.java
+++ src/main/java/net/javaguides/springbootsecurity/web/AccountConroller.java
@@ -1,5 +1,6 @@
 package net.javaguides.springbootsecurity.web;
 
+
 import net.javaguides.springbootsecurity.entities.Role;
 import net.javaguides.springbootsecurity.entities.User;
 import net.javaguides.springbootsecurity.repositories.RoleRepository;
@@ -15,8 +16,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import net.javaguides.springbootsecurity.entities.Message;
 import net.javaguides.springbootsecurity.repositories.MessageRepository;
 import net.javaguides.springbootsecurity.repositories.UserRepository;
+
 import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,30 +28,38 @@ import java.util.Optional;
  *
  */
 @Controller
-public class HomeController
+public class AccountConroller
 {
-	@Autowired
-	private MessageRepository messageRepository;
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private RoleRepository roleRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
-	@Autowired
-	public PasswordEncoder passwordEncoder;
+    @Autowired
+    public PasswordEncoder passwordEncoder;
 
-	@GetMapping("/home")
-	public String home(Model model)
-	{
-		model.addAttribute("msgs", messageRepository.findAll());		
-		return "userhome";
-	}
-	
-	@PostMapping("/messages")
-	public String saveMessage(Message message)
-	{
-		messageRepository.save(message);
-		return "redirect:/home";
-	}
+    @GetMapping("/register")
+    public String GetRegister()
+    {
+        return "register";
+    }
+    @PostMapping("/register")
+    public String Register(User user)
+    {
+        String pass = passwordEncoder.encode(user.getPassword());
+        user.setPassword(pass);
+        User u = new User();
+        u.setPassword(pass);
+        u.setEmail(user.getEmail());
+        u.setName(user.getName());
+        //Role r = roleRepository.findByName("ROLE_USER");
+//        Role r = new Role();
+//        r.setId(3);
+
+        u.setRoles(Arrays.asList(roleRepository.findByName("ROLE_USER")));
+        userRepository.save(u);
+        //messageRepository.save(message);
+        return "redirect:/home";
+    }
 }
