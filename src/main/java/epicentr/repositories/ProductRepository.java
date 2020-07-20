348dc4a82674fd9fdbfe71d93f0457f66197b1b0
Volodymyr Novak <novakvova@gmail.com>
2020-07-20 15:59:20 +0300

    Add app vuejs

--- src/main/java/epicentr/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package epicentr.repositories;
-
-import epicentr.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-}
