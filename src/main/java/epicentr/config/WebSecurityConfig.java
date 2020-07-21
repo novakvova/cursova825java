3ed43252182b80febe2d366d65a2c4935267d831
Volodymyr Novak <novakvova@gmail.com>
2020-07-21 15:08:36 +0300

    text commit

--- src/main/java/epicentr/config/WebSecurityConfig.java
+++ src/main/java/epicentr/config/WebSecurityConfig.java
@@ -50,6 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             	.antMatchers("/resources/**", "/webjars/**","/assets/**").permitAll()
                 .antMatchers("/").permitAll()
 				.antMatchers("/register").permitAll()
+				.antMatchers("/product").permitAll()
 				.antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
