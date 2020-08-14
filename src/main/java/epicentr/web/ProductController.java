7adfb84faa1f4e801786292a0fc8c673f47ea50e
Daniil <danik22122005@gmail.com>
2020-08-14 22:37:09 +0300

    product-view fix

--- src/main/java/epicentr/web/ProductController.java
+++ src/main/java/epicentr/web/ProductController.java
@@ -1,5 +1,8 @@
 package epicentr.web;
 
+import epicentr.repositories.ProductRespository;
+import epicentr.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +10,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
+    @Autowired
+    private ProductRespository productRespository;
     @GetMapping("/products")
     public String list(){
         return "products";
     }
+    //Old on Vue.js
+//    @GetMapping("/product-view/{product_id}")
+//    public String view(@PathVariable("product_id") int product_id,Model model){
+//        model.addAttribute("product_id",product_id);
+//        return "productview";
+//    }
+    //New on HTML
     @GetMapping("/product-view/{product_id}")
-    public String view(@PathVariable("product_id") int product_id,Model model){
-        model.addAttribute("product_id",product_id);
-        return "productview";
+    public String view(@PathVariable("product_id") Long product_id,Model model){
+        model.addAttribute("product",productRespository.findById(product_id).get());
+        return "productviewHTML";
     }
 }
 
