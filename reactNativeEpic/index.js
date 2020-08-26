3aba6c7d2e21a2826140096a2a643e67641abc7e
Volodymyr Novak <novakvova@gmail.com>
2020-08-26 16:50:44 +0300

    App class

--- reactNativeEpic/index.js
+++ reactNativeEpic/index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+const RNRedux = () => (
+    <App/>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
