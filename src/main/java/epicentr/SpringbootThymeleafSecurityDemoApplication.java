01529dceb4b792f31404e5931db71b75cacbabc0
Volodymyr Novak <novakvova@gmail.com>
2020-08-31 17:59:16 +0300

    Add MyWebfilter

--- src/main/java/epicentr/SpringbootThymeleafSecurityDemoApplication.java
+++ src/main/java/epicentr/SpringbootThymeleafSecurityDemoApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@ServletComponentScan
+@SpringBootApplication()
 @EnableConfigurationProperties(StorageProperties.class)
 public class SpringbootThymeleafSecurityDemoApplication extends SpringBootServletInitializer
 {
