a44ccebb99dad9b5bbe8d40fa92d800671da5f7c
Daniil <danik22122005@gmail.com>
2020-09-01 17:05:46 +0300

    jwtshka

--- src/main/java/epicentr/filters/MyWebFilter.java
+++ src/main/java/epicentr/filters/MyWebFilter.java
@@ -1,10 +1,19 @@
 package epicentr.filters;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import epicentr.constraint.SecurityConstants;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static epicentr.constraint.SecurityConstants.*;
 
 @WebFilter(urlPatterns = "/register")
 public class MyWebFilter implements Filter {
@@ -15,13 +24,40 @@ public class MyWebFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("filter for greeting project");
-//        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-//        httpResponse.sendRedirect("/login");
+        System.out.println("-----------filter for greeting project");
+        HttpServletRequest  req = (HttpServletRequest ) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String header = req.getHeader(HEADER_STRING);
+
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            filterChain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(req, res);
+        //httpResponse.sendRedirect("/login");
         filterChain.doFilter(servletRequest,servletResponse);
 
     }
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_STRING);
+        if (token != null) {
+            // parse the token.
+            String user = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
+                    .build()
+                    .verify(token.replace(TOKEN_PREFIX, ""))
+                    .getSubject();
 
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
     @Override
     public void destroy() {
 
