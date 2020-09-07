3df63c0019a106be9fba723d179270ace29c9e7f
Daniil <danik22122005@gmail.com>
2020-09-07 14:19:45 +0300

    order from react native

--- reactNativeEpic/components/ProductView/ProductView.js
+++ reactNativeEpic/components/ProductView/ProductView.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Card, ListItem, Image, Button,Badge} from 'react-native-elements';
+import {View, Text, TextInput} from 'react-native';
+import {Card, ListItem, Image, Button, Badge} from 'react-native-elements';
 import {ScrollView} from 'react-native-gesture-handler';
 import {connect} from 'react-redux';
 import * as getListActions from './reducer';
@@ -13,9 +13,9 @@ class ProductView extends Component {
     title: 'Product',
   };
 
-  state = {};
+  state = {count: ''};
   componentDidMount() {
-    console.log("DDDDDD");
+    console.log('DDDDDD');
     const {route} = this.props;
     const {id} = route.params;
 
@@ -26,7 +26,7 @@ class ProductView extends Component {
     const {data, navigation} = this.props;
     const {route} = this.props;
     const {id} = route.params;
-    if(id != data.id){
+    if (id != data.id) {
       this.props.getInfo({id});
     }
     return (
@@ -41,38 +41,84 @@ class ProductView extends Component {
           />
         </View>
         <ScrollView>
-          <ScrollView   horizontal={true}>
+          <ScrollView horizontal={true}>
             {data.productImages != undefined ? (
               data.productImages.map(function (el) {
                 console.log(`${serverUrl}files/` + el.image_name);
                 return (
                   <Image
-                    style={{borderRadius:20,width: 200, height: 200,marginLeft:25}}
+                    style={{
+                      borderRadius: 20,
+                      width: 200,
+                      height: 200,
+                      marginLeft: 25,
+                    }}
                     source={{
                       uri: `${serverUrl}files/` + el.image_name,
                     }}
-                  /> 
+                  />
                 );
               })
-            ) : ( 
+            ) : (
               <React.Fragment></React.Fragment>
             )}
           </ScrollView>
-          <Text style={{marginLeft:25,fontSize:35,fontWeight:'bold',marginBottom:10}}>{data.name}</Text>
-          <View style={{flexDirection:"row",alignItems:"flex-end",marginLeft:25}}>
-            <Text style={{textDecorationLine:"line-through",fontSize:25}}>{data.price}</Text>
-            <View style={{flexDirection:"column",marginLeft:10}}>
-              <View style={{marginLeft:-25,flexDirection:"row-reverse"}}>
-                <Badge value={"-"+data.discount+"%"} status="error" />
+          <Text
+            style={{
+              marginLeft: 25,
+              fontSize: 35,
+              fontWeight: 'bold',
+              marginBottom: 10,
+            }}>
+            {data.name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'flex-end',
+              marginLeft: 25,
+            }}>
+            <Text style={{textDecorationLine: 'line-through', fontSize: 25}}>
+              {data.price}
+            </Text>
+            <View style={{flexDirection: 'column', marginLeft: 10}}>
+              <View style={{marginLeft: -25, flexDirection: 'row-reverse'}}>
+                <Badge value={'-' + data.discount + '%'} status="error" />
               </View>
-              <Text  style={{color:"red",fontSize:25}}>{data.price - (data.price / 100 * data.discount)}</Text>
-
+              <Text style={{color: 'red', fontSize: 25}}>
+                {data.price - (data.price / 100) * data.discount}
+              </Text>
             </View>
-          </View> 
-          <Text style={{marginLeft:25,marginTop:10,fontSize:15}}>Description:</Text>
-          <Text style={{marginLeft:25,marginTop:10,fontSize:12}}>{data.description}</Text>
-
-        </ScrollView> 
+          </View>
+          <Text style={{marginLeft: 25, marginTop: 10, fontSize: 15}}>
+            Description:
+          </Text>
+          <Text style={{marginLeft: 25, marginTop: 10, fontSize: 12}}>
+            {data.description}
+          </Text>
+          {this.props.login.isAuthenticated ? (
+            <View style={{flexDirection: 'row', marginTop: 10}}>
+              <TextInput
+                style={{marginLeft: 25, marginRight: 5}}
+                placeholder="Count"
+                autoCompleteType="cc-number"
+                onChangeText={(value) => this.setState({count: value})}
+              />
+              <Button
+                title="Order"
+                onPress={(e) => {
+                  this.props.createOrder({
+                    username: this.props.login.user.sub,
+                    productId: data.id,
+                    count: this.state.count,
+                  });
+                  this.props.navigation.navigate('Home');
+                }}></Button>
+            </View>
+          ) : (
+            <></>
+          )}
+        </ScrollView>
       </React.Fragment>
     );
   }
@@ -83,12 +129,16 @@ function mapStateToProps(state) {
     failed: get(state, 'productView.list.failed'),
     errors: get(state, 'productView.list.errors'),
     data: get(state, 'productView.list.data'),
+    login: get(state, 'login'),
   };
 }
 const mapDispatchToProps = (dispatch) => {
   return {
     getInfo: (model) => {
       dispatch(getListActions.getInfo(model));
+    },
+    createOrder: (model) => {
+      dispatch(getListActions.createOrder(model));
     },
   };
 };
