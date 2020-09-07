3f82ceff26f4b62c552779e4766562455e7ddfa7
Daniil <danik22122005@gmail.com>
2020-09-07 14:00:18 +0300

    get my orders api

--- src/main/java/epicentr/api/OrderAPI.java
+++ src/main/java/epicentr/api/OrderAPI.java
@@ -1,6 +1,7 @@
 package epicentr.api;
 
 import epicentr.entities.Order;
+import epicentr.entities.User;
 import epicentr.models.CreateOrder;
 import epicentr.models.JwtRequest;
 import epicentr.models.JwtResponse;
@@ -10,11 +11,16 @@ import epicentr.repositories.ProductRespository;
 import epicentr.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -36,5 +42,23 @@ public class OrderAPI {
         o.setProduct(productRespository.getOne(new Long(model.getProductId())));
         orderRepository.save(o);
         return ResponseEntity.ok("ok");
+    }
+    @RequestMapping(value = "/myorders", method = RequestMethod.POST)
+    public ResponseEntity<List<Order>> myorders(@RequestBody CreateOrder model) {
+//        Authentication a = SecurityContextHolder.getContext().getAuthentication();
+//        String u = a.getName();
+        User user = userRepository.findByEmail(model.getUsername());
+        List<Order> orders = user.getOrders();
+        for (int i = 0; i < orders.size(); i++) {
+            int s = orders.get(i).getProduct().getProductImages().size();
+            orders.get(i).getProduct().setOrders(null);
+            orders.get(i).getOrderStatus().setOrders(null);
+            orders.get(i).setUser(null);
+            for (int j = 0; j < s; j++) {
+                orders.get(i).getProduct().getProductImages().get(j).setProduct(null);
+            }
+        }
+
+        return ResponseEntity.ok(orders);
     }
 }
